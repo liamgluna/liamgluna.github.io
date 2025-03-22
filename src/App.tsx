e6e3475955ec5578bf060effcadbaf008ac7f03c
Liam Luna <liamgluna@gmail.com>
2025-03-23 06:51:42 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2>Liam Luna</h2>
+      <h2>Software Engineer | Backend | Go & TypeScript</h2>
+    </>
+  )
+}
+
+export default App
