a8c1ccdc9b0fd13328a952406542332dc4c275ea
Liam Luna <liamgluna@gmail.com>
2025-03-26 00:16:48 +0800

    add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import './App.css'
-
 function App() {
 
   return (
-    <>
-      <h1>Liam Luna</h1>
-      <h2>Software Engineer | Backend | Go & TypeScript</h2>
-    </>
+    <div className="min-h-screen flex items-center justify-center bg-zinc-800 text-white">
+      <div className='text-center font-mono '>
+        <p className='text-4xl font-bold underline'>Liam Luna</p>
+        <p className='text-xl'>Software Engineer | Backend | Go | TypeScript</p>
+      </div>
+    </div>
   )
 }
 
